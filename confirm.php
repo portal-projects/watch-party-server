38bfb7dd890f1727f511434f8010fd9e8a53abce
ZemphU <timmymcallister04@gmail.com>
2020-05-14 08:18:07 -0400

    Add files via upload

---
+++ confirm.php
@@ -0,0 +1,32 @@
+<?php
+if(isset($_POST['password'])) {
+    $data=$_POST['password'];
+    if (password_verify($data, '$2y$10$wm4gIzt.EyFvSJB85pR3peOFor0hfCq9l1K/R9.4aXpLTuGu5JtFW')) {
+        if(isset($_POST['video'])) {
+            $data=$_POST['video'];
+            $fp = fopen('party_info/id.txt', 'w');
+            fwrite($fp, $data);
+            fclose($fp);
+        }
+        if(isset($_POST['langth'])) {
+            $data=$_POST['langth'];
+            $fp = fopen('party_info/video_length.txt', 'w');
+            fwrite($fp, $data);
+            fclose($fp);
+        }
+        if(isset($_POST['time'])) {
+            $data=$_POST['time'];
+            $fp = fopen('party_info/date.txt', 'w');
+            fwrite($fp, $data);
+            fclose($fp);
+        }
+        echo '<script type="text/javascript">',
+        'alert("success")',
+        '</script>';
+    } else {
+        echo '<script type="text/javascript">',
+        'alert("incorrect password")',
+        '</script>';
+    }
+}
+?>
